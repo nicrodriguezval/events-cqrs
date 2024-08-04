ea9c2d7d02e3b4cc8099d7e170a66d68bc63c900
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2024-08-03 19:30:30 -0500

    Search through elasticsearch

--- search/elastic.go
+++ search/elastic.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 
 	elastic "github.com/elastic/go-elasticsearch/v8"
 	"github.com/nicrodriguezval/cqrs/models"
@@ -39,4 +40,64 @@ func (r *ElasticSearchRepository) IndexFeed(ctx context.Context, feed models.Fee
 	)
 
 	return err
+}
+
+func (r *ElasticSearchRepository) SearchFeed(ctx context.Context, query string) (results []models.Feed, err error) {
+	var buf bytes.Buffer
+
+	searchQuery := map[string]any{
+		"query": map[string]any{
+			"multi_match": map[string]any{
+				"query":            query,
+				"fields":           []string{"title", "description"},
+				"fuzziness":        3,      // Allow up to 3 characters to be different
+				"cutoff_frequency": 0.0001, // Ignore terms that appear in more than 0.01% of documents
+			},
+		},
+	}
+	if err := json.NewEncoder(&buf).Encode(searchQuery); err != nil {
+		return nil, err
+	}
+
+	res, err := r.client.Search(
+		r.client.Search.WithContext(ctx),
+		r.client.Search.WithIndex("feeds"),
+		r.client.Search.WithBody(&buf),
+		r.client.Search.WithTrackTotalHits(true),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			results = nil
+		}
+	}()
+
+	if res.IsError() {
+		return nil, errors.New(res.String())
+	}
+
+	var eRes map[string]any
+	if err := json.NewDecoder(res.Body).Decode(&eRes); err != nil {
+		return nil, err
+	}
+
+	for _, hit := range eRes["hits"].(map[string]any)["hits"].([]any) {
+		source := hit.(map[string]any)["_source"].(map[string]any)
+		sourceBuf, err := json.Marshal(source)
+		if err != nil {
+			return nil, err
+		}
+
+		var feed models.Feed
+		if err := json.Unmarshal(sourceBuf, &feed); err != nil {
+			return nil, err
+		}
+
+		results = append(results, feed)
+	}
+
+  return results, nil
 }
